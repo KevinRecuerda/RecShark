7519f58a668a1d6b8b0704f7e687c74576b5cede
Gafs63 <68731384+Gafs63@users.noreply.github.com>
2020-07-24 20:03:43 +0200

    Wild card expression management

--- src/Testing.NSubstitute/LogExtensions.cs
+++ src/Testing.NSubstitute/LogExtensions.cs
@@ -9,88 +9,63 @@ namespace RecShark.Extensions.Testing.NSubstitute
 
     public static class LogExtensions
     {
-        public static void Logged(this ILogger logger, LogLevel? level = null, string message = null, int count = 1)
+        public static void Logged(this ILogger logger, LogLevel? level = null, string wildcardExpression = null, int count = 1)
         {
-            logger.Logged(null, level, FormattedLogValuesComparable.Is(message), count);
-        }
-
-        public static void LoggedMatching(this ILogger logger, LogLevel? level = null, string pattern = null, int count = 1)
-        {
-            logger.Logged(null, level, FormattedLogValuesComparable.Matchs(pattern), count);
+            logger.Logged(null, level, wildcardExpression, count);
         }
 
         public static void DidNotLog(
             this ILogger logger,
-            LogLevel?    level     = null,
-            string       message   = null,
-            Exception    exception = null)
+            LogLevel?    level              = null,
+            string       wildcardExpression = null,
+            Exception    exception          = null)
         {
-            logger.Logged(exception, level, FormattedLogValuesComparable.Is(message), 0);
-        }
-
-        public static void DidNotLogMatching(
-            this ILogger logger,
-            LogLevel?    level     = null,
-            string       pattern   = null,
-            Exception    exception = null)
-        {
-            logger.Logged(exception, level, FormattedLogValuesComparable.Matchs(pattern), 0);
+            logger.Logged(exception, level, wildcardExpression, 0);
         }
 
         private static void Logged(
-            this ILogger                 logger,
-            Exception                    exception,
-            LogLevel?                    level              = null,
-            FormattedLogValuesComparable formattedLogValues = null,
-            int                          count              = 1)
+            this ILogger logger,
+            Exception    exception,
+            LogLevel?    level              = null,
+            string       wildcardExpression = null,
+            int          count              = 1)
         {
             logger.Received(count)
                   .Log(
                        level ?? Arg.Any<LogLevel>(),
                        Arg.Any<EventId>(),
-                       formattedLogValues ?? Arg.Any<object>(),
+                       wildcardExpression != null ? new FormattedLogValuesComparable(wildcardExpression) : Arg.Any<object>(),
                        exception,
                        Arg.Any<Func<object, Exception, string>>());
         }
 
         private class FormattedLogValuesComparable
         {
-            public static FormattedLogValuesComparable Is(string message)
+            public FormattedLogValuesComparable(string wildcardExpression)
             {
-                return message != null
-                           ? new FormattedLogValuesComparable(message, (s1, s2) => s1 == s2)
-                           : null;
+                this.WildcardExpression = wildcardExpression;
             }
 
-            public static FormattedLogValuesComparable Matchs(string pattern)
-            {
-                return pattern != null
-                           ? new FormattedLogValuesComparable(pattern, (s1, s2) => new Regex(pattern).IsMatch(s1.ToString()))
-                           : null;
-            }
-
-            public FormattedLogValuesComparable(string message, Func<string, string, bool> equalityComparer)
-            {
-                Message          = message;
-                EqualityComparer = equalityComparer;
-            }
-
-            public Func<string, string, bool> EqualityComparer;
-            public string                     Message { get; }
+            public string WildcardExpression { get; }
 
             public override bool Equals(object obj)
             {
-                return obj is FormattedLogValues other && EqualityComparer(other.ToString(), this.Message);
+                return obj is FormattedLogValues other && Regex.IsMatch(other.ToString(), ConvertWildcardToRegEx(this.WildcardExpression));
             }
 
             public override int GetHashCode()
             {
-                return Message.GetHashCode();
+                return this.WildcardExpression.GetHashCode();
             }
 
             public override string ToString()
             {
-                return Message;
+                return this.WildcardExpression;
+            }
+
+            private static string ConvertWildcardToRegEx(string wildcardExpression)
+            {
+                return "^" + Regex.Escape(wildcardExpression).Replace("\\*", ".*").Replace("\\?", ".") + "$";
             }
         }
     }
