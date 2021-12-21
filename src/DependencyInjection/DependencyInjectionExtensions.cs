d8cab437e06aab4ec2ae625edd469ceef5c94e29
Kevin Recuerda <kevin.recuerda@gmail.com>
2021-12-21 08:17:03 +0100

    di - config extension (#26)

--- src/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/DependencyInjection/DependencyInjectionExtensions.cs
@@ -1,11 +1,47 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace RecShark.DependencyInjection
 {
     public static class DependencyInjectionExtensions
     {
+        private static IConfiguration config;
+
+        public static T AddConfig<T>(this IServiceCollection services, string name)
+            where T : class
+        {
+            // cache config for performance
+            // it should have no impact, as config should be loaded at first
+            config ??= services.BuildServiceProvider().GetService<IConfiguration>();
+
+            var section = config.GetSection(name).Get<T>();
+            services.TryAddSingleton(section, true);
+            return section;
+        }
+
+        public static IServiceCollection TryAddSingleton<T>(this IServiceCollection services, T instance, bool ignoreNull)
+            where T : class
+        {
+            if (ignoreNull && instance == null)
+                return services;
+
+            services.TryAddSingleton(instance);
+            return services;
+        }
+
+        public static void AddSingleton<TService1, TService2, TImplementation>(this IServiceCollection services)
+            where TImplementation : class, TService1, TService2
+            where TService1 : class
+            where TService2 : class
+        {
+            services.AddSingleton<TImplementation>();
+            services.AddSingleton<TService1, TImplementation>(x => x.GetRequiredService<TImplementation>());
+            services.AddSingleton<TService2, TImplementation>(x => x.GetRequiredService<TImplementation>());
+        }
+
         public static void Reset(this IServiceCollection services, ServiceDescriptor serviceDescriptor)
         {
             services.Remove(serviceDescriptor.ServiceType);
@@ -36,31 +72,11 @@ namespace RecShark.DependencyInjection
                 services.Remove(serviceDescriptor);
         }
 
-        public static void AddSingleton<TService1, TService2, TImplementation>(this IServiceCollection services)
-            where TImplementation : class, TService1, TService2
-            where TService1 : class
-            where TService2 : class
-        {
-            services.AddSingleton<TImplementation>();
-            services.AddSingleton<TService1, TImplementation>(x => x.GetRequiredService<TImplementation>());
-            services.AddSingleton<TService2, TImplementation>(x => x.GetRequiredService<TImplementation>());
-        }
-
-        public static IServiceCollection AddSingletonIfNotNull<TImplementation>(
-            this IServiceCollection services,
-            TImplementation implementationInstance)
-            where TImplementation : class
-        {
-            if (implementationInstance != null)
-                services.AddSingleton(implementationInstance);
-            return services;
-        }
-
         public static Type GetImplementationType(ServiceDescriptor serviceDescriptor)
         {
             return serviceDescriptor.ImplementationType
-                   ?? serviceDescriptor.ImplementationInstance?.GetType()
-                   ?? serviceDescriptor.ImplementationFactory?.GetType().GenericTypeArguments[1];
+                ?? serviceDescriptor.ImplementationInstance?.GetType()
+                ?? serviceDescriptor.ImplementationFactory?.GetType().GenericTypeArguments[1];
         }
     }
 }
