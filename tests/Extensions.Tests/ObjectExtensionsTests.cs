8226002e8f539e3186bb7abbfcddb3d4740d9000
Kevin Recuerda <kevin.recuerda@gmail.com>
2022-01-20 11:17:32 +0100

    extensions - use fastdeepcloner (#30)

--- tests/Extensions.Tests/ObjectExtensionsTests.cs
+++ tests/Extensions.Tests/ObjectExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using Xunit;
 
 namespace RecShark.Extensions.Tests
@@ -9,15 +10,34 @@ namespace RecShark.Extensions.Tests
         public void Clone__Should_clone_object()
         {
             // Arrange
-            var expected = new ObjectToClone { Index = 1, Message = "Hello World !" };
+            var item = new ObjectToClone { Index = 1, Message = "Hello World !" };
 
             // Act
-            var actual = expected.Clone();
-            expected.Message = "Message changed !";
+            var actual = item.Clone();
+            item.Message = "Message changed !";
 
             // Assert
-            actual.Should().NotBeEquivalentTo(expected);
+            actual.Should().NotBeEquivalentTo(item);
             actual.Message.Should().Be("Hello World !");
+        }
+
+        [Fact]
+        public void Clone__Should_clone_dico()
+        {
+            // Arrange
+            var item = new Dictionary<string, ObjectToClone>
+            {
+                ["A"] = new ObjectToClone { Index = 1, Message = "Hello World !" },
+                ["B"] = new ObjectToClone { Index = 2, Message = "Hello Team !" }
+            };
+
+            // Act
+            var actual = item.Clone();
+            item["A"].Message = "Message changed !";
+
+            // Assert
+            actual.Should().NotBeSameAs(item);
+            actual["A"].Message.Should().Be("Hello World !");
         }
 
         private class ObjectToClone
