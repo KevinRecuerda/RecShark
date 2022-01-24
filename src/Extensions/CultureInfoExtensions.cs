d2387a108310263355a40f9e51a68457e94b9a26
Kevin RECUERDA <kevin.recuerda@lyxor.com>
2022-01-24 12:05:38 +0100

    testing - specflow - handle parallel exec

---
+++ src/Extensions/CultureInfoExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace RecShark.Extensions
+{
+    public static class CultureInfoExtensions
+    {
+        public static void Use(this CultureInfo cultureInfo)
+        {
+            CultureInfo.CurrentCulture   = cultureInfo;
+            CultureInfo.CurrentUICulture = cultureInfo;
+        }
+
+        public static void UseDefault(this CultureInfo cultureInfo)
+        {
+            cultureInfo.Use();
+            CultureInfo.DefaultThreadCurrentCulture   = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        }
+    }
+}
---
+++ src/Testing/SpecFlow/BasicSteps.cs
@@ -0,0 +1,14 @@
+﻿using RecShark.Extensions;
+using TechTalk.SpecFlow;
+
+namespace RecShark.Testing.SpecFlow
+{
+    public abstract class BasicSteps : Steps
+    {
+        static BasicSteps()
+        {
+            typeof(Tests).RunStaticConstructor();
+            SpecFlowExtensions.UseObjectConverter();
+        }
+    }
+}
