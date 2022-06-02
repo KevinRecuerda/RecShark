4d1cef42b74b72f7158fc4c4b513f9e37eb39d79
fontanes <sylvain.fontanes@amundi.com>
2022-06-02 16:14:51 +0200

    Add polly retry

--- src/AspNetCore.ApiClient/HttpClientFactoryExtensions.cs
+++ src/AspNetCore.ApiClient/HttpClientFactoryExtensions.cs
@@ -1,36 +1,49 @@
 namespace RecShark.AspNetCore.ApiClient
 {
     using System;
+    using System.Collections.Generic;
     using System.Net;
     using System.Net.Http;
     using Microsoft.Extensions.DependencyInjection;
+    using Polly;
     using RecShark.AspNetCore.ApiClient.Security;
     using RecShark.DependencyInjection;
     using Refit;
 
     public static class HttpClientFactoryExtensions
     {
+        public static ICollection<Func<PolicyBuilder<HttpResponseMessage>, IAsyncPolicy<HttpResponseMessage>>> DefaultPolicies =
+            new List<Func<PolicyBuilder<HttpResponseMessage>, IAsyncPolicy<HttpResponseMessage>>>()
+            {
+                // Retry
+                builder => builder.WaitAndRetryAsync(new[] {TimeSpan.FromMilliseconds(500)}),
+            };
+
         public static IHttpClientBuilder AddApiClient<T, TImpl>(
             this IServiceCollection services,
             ApiClientConfig         config,
-            Action<HttpClient>      configure = null)
+            Action<HttpClient>      configure          = null,
+            bool                    useDefaultPolicies = true)
             where T : class
             where TImpl : class, T
         {
             return services.AddClientConfig<T>(config)
                            .AddHttpClient<T, TImpl>()
-                           .ConfigureClient(config, configure);
+                           .ConfigureClient(config, configure)
+                           .AddErrorPolicies(useDefaultPolicies);
         }
 
         public static IHttpClientBuilder AddRefitApiClient<T>(
             this IServiceCollection services,
             ApiClientConfig         config,
-            Action<HttpClient>      configure = null)
+            Action<HttpClient>      configure          = null,
+            bool                    useDefaultPolicies = true)
             where T : class
         {
             return services.AddClientConfig<T>(config)
                            .AddRefitClient<T>()
-                           .ConfigureClient(config, configure);
+                           .ConfigureClient(config, configure)
+                           .AddErrorPolicies(useDefaultPolicies);
         }
 
         public static IServiceCollection AddClientConfig<T>(this IServiceCollection services, ApiClientConfig config)
@@ -41,6 +54,16 @@ namespace RecShark.AspNetCore.ApiClient
 
             services.Load<SecurityModule>();
             return services;
+        }
+
+        private static IHttpClientBuilder AddErrorPolicies(this IHttpClientBuilder builder, bool useDefaultPolicies)
+        {
+            if (useDefaultPolicies)
+            {
+                foreach (var defaultPolicy in DefaultPolicies)
+                    builder = builder.AddTransientHttpErrorPolicy(defaultPolicy);
+            }
+            return builder;
         }
 
         private static IHttpClientBuilder ConfigureClient(this IHttpClientBuilder builder, ApiClientConfig config, Action<HttpClient> configure)
@@ -68,11 +91,11 @@ namespace RecShark.AspNetCore.ApiClient
                     var credentials = new NetworkCredential(config.UserName, config.Password);
                     var proxy       = new WebProxy(config.Url, true, null, credentials);
                     return new HttpClientHandler()
-                    {
-                        Proxy                 = proxy,
-                        UseDefaultCredentials = false,
-                        PreAuthenticate       = true
-                    };
+                           {
+                               Proxy                 = proxy,
+                               UseDefaultCredentials = false,
+                               PreAuthenticate       = true
+                           };
                 });
             return builder;
         }
