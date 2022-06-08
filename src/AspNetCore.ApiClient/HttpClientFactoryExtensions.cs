6a2c6cd18cd61e3481ac2fde6802125107f04ebe
Gafs63 <68731384+Gafs63@users.noreply.github.com>
2022-06-08 11:18:04 +0200

    api - polly retry (#54)
    * Add polly retry
    
    * CR
    
    * CRR
    
    * CR - renaming
    
    * add circuit breaker
    
    * CR
    
    Co-authored-by: fontanes <sylvain.fontanes@amundi.com>
    Co-authored-by: Kevin RECUERDA <kevin.recuerda@amundi.com>

--- src/AspNetCore.ApiClient/HttpClientFactoryExtensions.cs
+++ src/AspNetCore.ApiClient/HttpClientFactoryExtensions.cs
@@ -4,54 +4,71 @@ namespace RecShark.AspNetCore.ApiClient
     using System.Net;
     using System.Net.Http;
     using Microsoft.Extensions.DependencyInjection;
+    using Polly;
     using RecShark.AspNetCore.ApiClient.Security;
     using RecShark.DependencyInjection;
     using Refit;
 
     public static class HttpClientFactoryExtensions
     {
+        public static readonly Action<IHttpClientBuilder> DefaultPolicy =
+            builder => builder.AddTransientHttpErrorPolicy(x => x.WaitAndRetryAsync(new[] {TimeSpan.FromMilliseconds(500)}))
+                              .AddTransientHttpErrorPolicy(x => x.CircuitBreakerAsync(3, TimeSpan.FromSeconds(10)));
+
         public static IHttpClientBuilder AddApiClient<T, TImpl>(
             this IServiceCollection services,
             ApiClientConfig         config,
-            Action<HttpClient>      configure = null)
+            Action<HttpClient>      configure        = null,
+            bool                    useDefaultPolicy = true)
             where T : class
             where TImpl : class, T
         {
             return services.AddClientConfig<T>(config)
                            .AddHttpClient<T, TImpl>()
-                           .ConfigureClient(config, configure);
+                           .ConfigureClient(config, configure)
+                           .AddDefaultPolicy(useDefaultPolicy);
         }
 
         public static IHttpClientBuilder AddRefitApiClient<T>(
             this IServiceCollection services,
             ApiClientConfig         config,
-            Action<HttpClient>      configure = null)
+            Action<HttpClient>      configure        = null,
+            bool                    useDefaultPolicy = true)
             where T : class
         {
             return services.AddClientConfig<T>(config)
                            .AddRefitClient<T>()
-                           .ConfigureClient(config, configure);
+                           .ConfigureClient(config, configure)
+                           .AddDefaultPolicy(useDefaultPolicy);
         }
 
         public static IServiceCollection AddClientConfig<T>(this IServiceCollection services, ApiClientConfig config)
             where T : class
         {
-            var injectableConfig = new ApiClientConfig<T>() { Value = config };
+            var injectableConfig = new ApiClientConfig<T>() {Value = config};
             services.AddSingleton<IApiClientConfig<T>>(injectableConfig);
 
             services.Load<SecurityModule>();
             return services;
         }
 
+        public static IHttpClientBuilder AddDefaultPolicy(this IHttpClientBuilder builder, bool useDefaultPolicy = true)
+        {
+            if (useDefaultPolicy)
+                DefaultPolicy(builder);
+
+            return builder;
+        }
+
         private static IHttpClientBuilder ConfigureClient(this IHttpClientBuilder builder, ApiClientConfig config, Action<HttpClient> configure)
         {
             builder = builder.ConfigureHttpClient(
-                                  client =>
-                                  {
-                                      client.BaseAddress = new Uri(config.Url);
-                                      client.SetAuthorization(config.Security);
-                                      configure?.Invoke(client);
-                                  })
+                                 client =>
+                                 {
+                                     client.BaseAddress = new Uri(config.Url);
+                                     client.SetAuthorization(config.Security);
+                                     configure?.Invoke(client);
+                                 })
                              .AddAuthorizationHandler(config.Code, config.Security)
                              .AddProxy(config.Proxy);
             return builder;
