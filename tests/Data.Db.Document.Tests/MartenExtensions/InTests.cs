b4dacee819411a1c3a24da5da46a30ff23092b1e
Gafs63 <68731384+Gafs63@users.noreply.github.com>
2022-11-23 12:15:14 +0100

    All projects in net6

--- tests/Data.Db.Document.Tests/MartenExtensions/InTests.cs
+++ tests/Data.Db.Document.Tests/MartenExtensions/InTests.cs
@@ -3,11 +3,11 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Marten;
 using Microsoft.Extensions.DependencyInjection;
-using RecShark.Extensions;
 using Xunit;
 
 namespace RecShark.Data.Db.Document.Tests.MartenExtensions
 {
+    //TODO: remove (In is now implemented by Marten)
     public class InTests : BaseDocTests
     {
         public InTests(DocHooks hooks = null) : base(hooks) { }
@@ -34,23 +34,51 @@ namespace RecShark.Data.Db.Document.Tests.MartenExtensions
             await session.SaveChangesAsync();
 
             // Act & Assert
-            await AssertIn(session, null,             null,               items[0], items[1], items[2]);
-            await AssertIn(session, null,             new ItemType[] { }, items[0], items[1], items[2]);
-            await AssertIn(session, new string[] { }, null,               items[0], items[1], items[2]);
+            await AssertIn(session, null,             null,               null, items[0], items[1], items[2]);
+            await AssertIn(session, null,             new ItemType[] { }, null, items[0], items[1], items[2]);
+            await AssertIn(session, new string[] { }, null,               null, items[0], items[1], items[2]);
             
-            await AssertIn(session, new[] {"10", "30"}, null, items[0], items[2]);
-            await AssertIn(session, new[] {"40"});
+            await AssertIn(session, new[] {"10", "30"}, null, null, items[0], items[2]);
+            await AssertIn(session, new[] {"40"},       null);
 
-            await AssertIn(session, null, new[] {ItemType.B},             items[2]);
-            await AssertIn(session, null, new[] {ItemType.A, ItemType.B}, items[0], items[1], items[2]);
+            await AssertIn(session, null, new[] {ItemType.B},             null, items[2]);
+            await AssertIn(session, null, new[] {ItemType.A, ItemType.B}, null, items[0], items[1], items[2]);
 
-            await AssertIn(session, new[] {"10"}, new[] {ItemType.A}, items[0]);
+            await AssertIn(session, new[] {"10"}, new[] {ItemType.A}, null, items[0]);
         }
 
-        private static async Task AssertIn(IQuerySession session, string[] ids = null, ItemType[] types = null, params object[] expected)
+        [Fact]
+        public async Task In__Should_manage_null_values()
+        {
+            // Arrange
+            var items = new[]
+            {
+                new Item {Id = "10", Name = "test 1", Type = ItemType.A},
+                new Item {Id = "20", Name = null, Type     = ItemType.A},
+                new Item {Id = "30", Name = "", Type       = ItemType.B},
+            };
+
+            var       documentStore = Hooks.Provider.GetService<IDocumentStore>();
+            using var session       = documentStore.OpenSession();
+            session.Store(items);
+            await session.SaveChangesAsync();
+
+            // Act & Assert
+            await AssertIn(session, null, null, new[] {"test 1"}, items[0]);
+            await AssertIn(session, null, null, new[] {""},       items[2]);
+            await AssertIn(session, null, null, new[] {(string) null});
+        }
+
+        private static async Task AssertIn(
+            IQuerySession   session,
+            string[]        ids   = null,
+            ItemType[]      types = null,
+            string[]        names = null,
+            params object[] expected)
         {
             var actual = await session.Query<Item>()
                                       .Where(x => x.Id.In(ids))
+                                      .Where(x => x.Name.In(names))
                                       .Where(x => x.Type.In(types))
                                       .ToListAsync();
 
